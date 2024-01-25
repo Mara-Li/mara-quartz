883af286597c4bf2968773f6f6ab9d47acc2e97c
Mara <mara-li@outlook.fr>
2024-01-25 19:41:52 +0100

    adjust custom css

--- content/index.md
+++ content/index.md
@@ -3,7 +3,7 @@ title: Owlly lab
 description: "Je suis Mara, j'ai 26 ans. Je suis étudiante en Santé Publique, et j'ai pour hobby la programmation, le Roleplay et les jeux-vidéos. Ce site me permet de présenter mes projets, mes personnages, mes univers et mes outils."
 ---
 
-> [!info]
+> [!objectif]
 > coucou
 
 > [!note]
--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -5,7 +5,7 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 export default (() => {
   function Head({ cfg, fileData, externalResources }: QuartzComponentProps) {
     const title = fileData.frontmatter?.title ?? "Untitled"
-    const description = fileData.description?.trim() ?? "No description provided"
+    const description = fileData.frontmatter?.description?.trim() ?? "No description provided"
     const { css, js } = externalResources
 
     const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
@@ -13,7 +13,7 @@ export default (() => {
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
     const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
+    const ogImagePath = fileData?.frontmatter?.image?.trim() ?? `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
       <head>
