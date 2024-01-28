bb4bf0ee576e55dfc2d6766543c478cfa7481f30
LUCASTUCIOUS <peterlucas2804@gmail.com>
2024-01-28 03:34:21 +0100

    feat: Handling cssclasses properties in Quartz (#711)
    * Add cssclasses to article
    
    * Prettier
    
    * Update quartz/components/pages/Content.tsx
    
    * Update quartz/components/pages/Content.tsx
    
    ---------
    
    Co-authored-by: Jacky Zhao <j.zhao2k19@gmail.com>

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -3,15 +3,9 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
 function Content({ fileData, tree }: QuartzComponentProps) {
   const content = htmlToJsx(fileData.filePath!, tree)
-  const frontmatter = fileData.frontmatter
-  let cssClasses = []
-  if (frontmatter?.cssclasses) {
-    cssClasses = frontmatter.cssclasses
-  } else if (frontmatter?.cssClasses) {
-    cssClasses = frontmatter.cssClasses
-  }
-  const css: string = cssClasses.join(" ")
-  return <article class={`popover-hint ${css}`}>{content}</article>
+  const classes: string[] = fileData.frontmatter?.cssclasses ?? []
+  const classString = ["popover-hint", ...classes].join(" ")
+  return <article class={classString}>{content}</article>
 }
 
 export default (() => Content) satisfies QuartzComponentConstructor
